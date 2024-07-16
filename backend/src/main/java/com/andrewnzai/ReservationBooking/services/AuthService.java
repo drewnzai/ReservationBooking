74306af6aa1da702f9d119c9521b029d825f63f1
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-16 14:58:12 +0300

    Updated user authentication

--- backend/src/main/java/com/andrewnzai/ReservationBooking/services/AuthService.java
+++ backend/src/main/java/com/andrewnzai/ReservationBooking/services/AuthService.java
@@ -1,7 +1,12 @@
 package com.andrewnzai.ReservationBooking.services;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.andrewnzai.ReservationBooking.repositories.RoleRepository;
 import com.andrewnzai.ReservationBooking.repositories.UserRepository;
+
+import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
