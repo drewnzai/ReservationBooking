98b1b888ba75b96c9815000572d2010f0ccf5c84
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-16 12:52:36 +0300

    Added RegisterRequest dto

---
+++ backend/src/main/java/com/andrewnzai/ReservationBooking/dtos/RegisterRequest.java
@@ -0,0 +1,16 @@
+package com.andrewnzai.ReservationBooking.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private String role;
+}
