e08ad053320d6bb287567d0da3053de247cc118a
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-16 11:07:13 +0300

    Added repositories

---
+++ backend/src/main/java/com/andrewnzai/ReservationBooking/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.andrewnzai.ReservationBooking.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andrewnzai.ReservationBooking.models.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
