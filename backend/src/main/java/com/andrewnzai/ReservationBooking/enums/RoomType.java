af99eec1c858ffb8d757bf5aa79368ab158cd6b5
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-22 16:17:52 +0300

    Created new enum

---
+++ backend/src/main/java/com/andrewnzai/ReservationBooking/enums/RoomType.java
@@ -0,0 +1,22 @@
+package com.andrewnzai.ReservationBooking.enums;
+
+public enum RoomType {
+    SINGLE,
+    DOUBLE,
+    TWIN,
+    TRIPLE,
+    QUAD,
+    QUEEN,
+    KING,
+    STUDIO,
+    SUITE,
+    JUNIOR_SUITE,
+    EXECUTIVE,
+    PRESIDENTIAL_SUITE,
+    ACCESSIBLE,
+    CONNECTING,
+    FAMILY,
+    DELUXE,
+    SUPERIOR,
+    PENTHOUSE
+}
