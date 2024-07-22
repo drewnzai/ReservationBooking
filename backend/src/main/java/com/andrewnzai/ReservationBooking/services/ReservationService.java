f4831321b1faf8b202d67f7c4e054f45a4d371ea
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-22 16:16:29 +0300

    Updated

--- backend/src/main/java/com/andrewnzai/ReservationBooking/services/ReservationService.java
+++ backend/src/main/java/com/andrewnzai/ReservationBooking/services/ReservationService.java
@@ -1,5 +1,6 @@
 package com.andrewnzai.ReservationBooking.services;
 
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,15 +20,22 @@ public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final RoomRepository roomRepository;
 
-    public Object searchForAvailable(ReservationRequest reservationRequest){
+    public List<AvailableRoom> searchForAvailable(ReservationRequest reservationRequest){
         List<Room> rooms = roomRepository.findAll();
         List<AvailableRoom> availableRooms = new ArrayList<>();
 
         for(Room room: rooms){
+            
             if(room.getAvailable() >= reservationRequest.getGuestsNo()){
+
+                Long days = ChronoUnit.DAYS.between(reservationRequest.getFromDate(), reservationRequest.getToDate());
                 AvailableRoom availableRoom = new AvailableRoom();
+                availableRoom.setRoomType(room.getName());
+                availableRoom.setDays(days);
+                availableRoom.setTotal(room.getPrice()* reservationRequest.getGuestsNo()* days);
+                availableRooms.add(availableRoom);
             }
         }
-        return null;
+        return availableRooms;
     }
 }
