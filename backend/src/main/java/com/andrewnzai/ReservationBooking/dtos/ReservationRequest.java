88dea1d0aa7f349ab522421677032b3b163a03fa
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-22 13:08:01 +0300

    Updated reservationRequest

---
+++ backend/src/main/java/com/andrewnzai/ReservationBooking/dtos/ReservationRequest.java
@@ -0,0 +1,20 @@
+package com.andrewnzai.ReservationBooking.dtos;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReservationRequest {
+    private Long guestsNo;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate toDate;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate fromDate;
+}
