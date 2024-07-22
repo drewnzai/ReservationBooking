e45a3874fc17098c1e0a0dae710126f5f71eb489
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-22 10:23:03 +0300

    Updated home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,29 @@
+import { Box } from "@mui/material";
 
 export default function Home(){
 
     return(
-        <div>
-            <h1>Welcome</h1>
-        </div>
+        <Box
+        sx={{
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "3px"
+        }}
+        >
+            <Box
+            mt={"200px"}
+            display="block"
+            width="300px"
+            gap="30px"
+            p={"10px"}
+            sx={{
+                alignItems: "center",
+                justifyContent: "center"
+
+            }}            
+            >
+                
+            </Box>
+        </Box>
     );
 }
