6dd1ca52fea0145f44a75e467d9a6f038e3cca86
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-23 10:03:44 +0300

    Update

--- backend/src/main/java/com/andrewnzai/ReservationBooking/enums/RoomType.java
+++ backend/src/main/java/com/andrewnzai/ReservationBooking/enums/RoomType.java
@@ -1,32 +1,22 @@
 package com.andrewnzai.ReservationBooking.enums;
 
 public enum RoomType {
-    SINGLE(1),
-    DOUBLE(2),
-    TWIN(2),
-    TRIPLE(3),
-    QUAD(4),
-    QUEEN(2),
-    KING(2),
-    STUDIO(2),
-    SUITE(4),
-    JUNIOR_SUITE(3),
-    EXECUTIVE(2),
-    PRESIDENTIAL_SUITE(6),
-    ACCESSIBLE(2),
-    CONNECTING(4),
-    FAMILY(4),
-    DELUXE(3),
-    SUPERIOR(2),
-    PENTHOUSE(6);
-
-    private final int optimalOccupants;
-
-    RoomType(int optimalOccupants) {
-        this.optimalOccupants = optimalOccupants;
-    }
-
-    public int getOptimalOccupants() {
-        return optimalOccupants;
-    }
+    SINGLE,
+    DOUBLE,
+    TWIN,
+    TRIPLE,
+    QUAD,
+    QUEEN,
+    KING,
+    STUDIO,
+    SUITE,
+    JUNIOR_SUITE,
+    EXECUTIVE,
+    PRESIDENTIAL_SUITE,
+    ACCESSIBLE,
+    CONNECTING,
+    FAMILY,
+    DELUXE,
+    SUPERIOR,
+    PENTHOUSE;
 }
