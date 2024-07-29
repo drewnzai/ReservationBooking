012499a08b900bad3ebdc2a301847d2334149010
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-29 12:03:01 +0300

    Created reservation service

---
+++ frontend/src/services/ReservationService.service.ts
@@ -0,0 +1,17 @@
+import { ReservationRequest } from "../models/ReservationRequest";
+import ApiInterceptor from "../auth/ApiInterceptor";
+
+const base_url = "http://localhost:8080/api/reservation/";
+
+export default class ReservationService{
+    searchForAvailableRooms(reservationRequest: ReservationRequest){
+        return ApiInterceptor.post("search", reservationRequest)
+            .then(
+                (response) => {
+                    if(response.data){
+                        return response.data;
+                    }
+                }
+            )
+    }
+}
