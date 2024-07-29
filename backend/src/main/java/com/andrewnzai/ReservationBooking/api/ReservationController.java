41b6cf7ec8d551fcca549e0e7581389335700644
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-29 10:59:49 +0300

    Create the search reservation endpoint

--- backend/src/main/java/com/andrewnzai/ReservationBooking/api/ReservationController.java
+++ backend/src/main/java/com/andrewnzai/ReservationBooking/api/ReservationController.java
@@ -1,10 +1,15 @@
 package com.andrewnzai.ReservationBooking.api;
 
-import org.springframework.http.ResponseEntity;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.andrewnzai.ReservationBooking.dtos.ReservationRequest;
+import com.andrewnzai.ReservationBooking.dtos.AvailableRoom;
+import com.andrewnzai.ReservationBooking.services.ReservationService;
 
 import lombok.AllArgsConstructor;
 
@@ -13,5 +18,10 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/reservation")
 @AllArgsConstructor
 public class ReservationController {
+    private final ReservationService reservationService;
 
+    @PostMapping("/search")
+    public List<AvailableRoom> searchForAvailableRooms(@RequestBody ReservationRequest reservationRequest){
+        return reservationService.searchForAvailable(reservationRequest);
+    }
 }
