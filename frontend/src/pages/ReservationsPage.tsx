2e2c2fdddb7e2f6d4df676d8a20e8fb7871f9a29
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-08-06 16:06:53 +0300

    Forgot

--- frontend/src/pages/ReservationsPage.tsx
+++ frontend/src/pages/ReservationsPage.tsx
@@ -1,18 +1,24 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Reservation } from "../models/Reservation";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function ReservationPage(){
+    const navigate = useNavigate();
     const location = useLocation();
     const {reservations} = location.state as { reservations: Reservation[] };
 
     return(
         <Box p={3}>
+            <Button variant="contained" sx={{
+                backgroundColor: "red"
+                }} onClick={() => navigate('/')}>
+                    Go back to Home
+          </Button>
             <Typography variant="h4" gutterBottom>
                 Your Reservations
             </Typography>
             {reservations.map((reservation, index) => (
-                <Box key={index} mb={2} p={2} border={1} borderRadius={1}>
+                <Box key={index} mb={2} p={2} border={1} borderRadius={2}>
                     <Typography variant="body1">
                         Reservation {index + 1}:
                     </Typography>
