2e8cebcea9fec8fa3b7dbbcbfb7dad24e422657f
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-08-13 15:48:20 +0300

    Added new cors origin

--- backend/src/main/java/com/andrewnzai/ReservationBooking/configs/SecurityConfig.java
+++ backend/src/main/java/com/andrewnzai/ReservationBooking/configs/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowedMethods(List.of("*"));
-        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
