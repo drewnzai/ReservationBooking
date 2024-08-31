43d4f70e3183108ea9e13d70865eb7cf72f246b7
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-08-31 11:46:15 +0300

    Added loading component

---
+++ frontend/admin/src/components/Loading.tsx
@@ -0,0 +1,21 @@
+import { CircularProgress, Box, Typography } from '@mui/material';
+
+const Loading = () => {
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            height="100vh"
+            width="100vw"
+        >
+            <CircularProgress color='secondary' />
+            <Typography variant="h6" mt={2}>
+                Loading...
+            </Typography>
+        </Box>
+    );
+};
+
+export default Loading;
