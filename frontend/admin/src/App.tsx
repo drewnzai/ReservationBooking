fa176a150e1c9a3812e6a653fef5ae28f9ca6564
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-08-31 11:53:58 +0300

    Added base functionality

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import { ColorModeContext, useMode } from './theme'
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from 'react-toastify';
 
-import './App.css'
 
 function App() {
+  const [theme, colorMode] = useMode();
 
   return (
     <>
+    {/*An error that's just a pain really, works fine*/}
+    {/* @ts-ignore */}
+    <ColorModeContext.Provider value={colorMode}>
+
+    {/*An error that's just a pain really, works fine*/}
+    {/* @ts-ignore */}
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
       
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        hideProgressBar
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        />
+    </ThemeProvider>
+    </ColorModeContext.Provider>      
     </>
   )
 }
