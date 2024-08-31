1a89fda2e0b3bd9d0229af6d9dd94c64efddca06
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-08-31 11:42:46 +0300

    Created models

---
+++ frontend/admin/src/models/Reservation.ts
@@ -0,0 +1,10 @@
+export interface Reservation {
+    id: number;
+    days: number;
+    roomType: string;
+    total: number;
+    occupants: number;
+    checkIn: string;
+    checkOut: string;
+    reservationDate: string;
+}
