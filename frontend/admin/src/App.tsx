e55342365d77a2a02fd3be43bf05eaca565288c0
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-08-31 11:59:03 +0300

    Added home page

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -2,6 +2,8 @@ import {CssBaseline, ThemeProvider} from '@mui/material';
 import { ColorModeContext, useMode } from './theme'
 import 'react-toastify/dist/ReactToastify.css';
 import {ToastContainer} from 'react-toastify';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
 
 
 function App() {
@@ -17,6 +19,13 @@ function App() {
     {/* @ts-ignore */}
     <ThemeProvider theme={theme}>
       <CssBaseline/>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      
+      </BrowserRouter>
       
       <ToastContainer
         position="top-right"
