913a6100b63a2553f53a48e2c55977722ae0c45f
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-09-05 11:33:05 +0300

    Added JSON formatter to localDate for correct parsing

--- backend/src/main/java/com/andrewnzai/ReservationBooking/dtos/ReservationDto.java
+++ backend/src/main/java/com/andrewnzai/ReservationBooking/dtos/ReservationDto.java
@@ -15,7 +15,9 @@ public class ReservationDto {
     private Long total;
     private Long days;
     private Integer occupants;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate checkIn;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate checkOut;
     private LocalDate reservationDate;
     private String reserver;
